e5a0cea0ca10b06a0a91c33862325b6140e28cde
Simon Almers <account@simonalmers.com>
2021-06-20 16:32:41 +0200

    Add test files and initial tests for users and projects

---
+++ planningpoker/users/tests/test_setup.py
@@ -0,0 +1,20 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+
+class TestSetUp(APITestCase):
+    def setUp(self):
+        self.register_url = reverse("register")
+        self.login_url = reverse("token_obtain_pair")
+        self.refresh_token_url = reverse("token_refresh")
+
+        self.user_data = {
+            "email": "first.last@test.com",
+            "first_name": "First",
+            "last_name": "Last",
+            "password": "supersecret",
+        }
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
